1c91e102bc27bed3707b16189bf8859b8a3ff021
hardee <shegurov.danil@gmail.com>
2023-01-17 17:32:20 +0400

    ref(integrations): refactored VT integration

--- integrations/virustotal/virustotal.py
+++ integrations/virustotal/virustotal.py
@@ -6,58 +6,64 @@ VT_KEY = "?"
 
 class VirusTotalIntegration:
     def __init__(self) -> None:
-        self.__elements = list()
         self.__window_object = None
         self.__binary_buffer = None
-        self.__loading_layer = None
+        self.__elements = list()
 
     def __vt_analyze_pressed(self) -> None:
         if self.__binary_buffer is None:
-            print("[!] no binary to send over to virustotal")
-
+            print("[!] load binary first")
             return
 
         self.__loading_layer.set_action('Performing VirusTotal scan')
-        self.__loading_layer.set_sub_action('Building URL')
 
-        self.__loading_layer = self.__window_object.get_element_by_alias('LOADING_LAYER').get()
-        
         dimensions = self.__loading_layer.get_dimensions()
+
         self.__loading_layer.get_tk_object().place(
-            x = dimensions[0], 
-            y = dimensions[1],
-            width = dimensions[2],
-            height = dimensions[3]
+            x = dimensions[0], y = dimensions[1],
+            width = dimensions[2], height = dimensions[3]
         )
         
-        url = "https://www.virustotal.com/api/v3/files/%s" % hashlib.sha256(self.__binary_buffer).hexdigest()
-
-        self.__loading_layer.set_sub_action('Sending request to %s' % url)
-
-        fetch_thread = threading.Thread(target=self.__fetch_analysis, args=[url])
-        fetch_thread.start()
+        url = f"https://www.virustotal.com/api/v3/files/{hashlib.sha256(self.__binary_buffer).hexdigest()}"
+        threading.Thread(target=self.__fetch_analysis, args=[url]).start()
 
     def __fetch_analysis(self, url):
-        response = requests.get(url, headers={ "x-apikey": VT_KEY })
+        self.__loading_layer.set_sub_action(f"Sending request")
+        
+        response = requests.get(
+            url, headers = { "x-apikey": VT_KEY }
+        )
 
-        self.__on_analysis_response_arrived(response)
+        self.__on_analysis_response_received(response)
 
-    def __on_analysis_response_arrived(self, response):
+    def __on_analysis_response_received(self, response):
         if response.status_code == 200:
             self.__on_analysis_completed(response.json())
-
-            self.__loading_layer.get_tk_object().place_forget()
         else:
-            print("[!] failed to fetch analysis data (%d): %s" % (response.status_code, response.text))
+            print(f"[!] failed to fetch analysis data ({response.status_code}): {response.text}")
 
-            self.__loading_layer.get_tk_object().place_forget()
+        self.__loading_layer.get_tk_object().place_forget()
 
     def __on_analysis_completed(self, analysis) -> None:
+        # todo: harden it against any potential API changes by checking if this keys exist before using them
         stats = analysis["data"]["attributes"]["last_analysis_stats"]
         results = analysis["data"]["attributes"]["last_analysis_results"]
 
-        self.__tk_label_vt_result.get_tk_object()["text"] = "Score: Safe: %d, Malicious: %d, Suspicious: %d, Undetected: %d, Failed: %d, Total: %d" % (stats["harmless"], stats["malicious"], stats["suspicious"], stats["undetected"], stats["confirmed-timeout"] + stats["type-unsupported"] + stats["failure"] + stats["timeout"], sum(stats.values()))
-        self.__tk_table_vt_results.update_data([["%s (%s)" % (result["engine_name"], result["engine_version"]), result["result"]] for result in results.values()])
+        failed_checks = stats["confirmed-timeout"] + stats["type-unsupported"] + stats["failure"] + stats["timeout"]
+
+        self.__label_vt_result.get_tk_object().configure(
+            text = f"Score: Safe: {stats['harmless']}; " \
+                f"Malicious: {stats['malicious']}; Suspicious: {stats['suspicious']}; " \
+                f"Undetected: {stats['undetected']}; Failed: {failed_checks}, Total: {sum(stats.values())}"
+        )
+
+        self.__table_vt_results.update_data(
+            [
+                ["%s (%s)" % (result["engine_name"], result["engine_version"]), result["result"]
+            ] for result in results.values()]
+        )
+
+        self.__table_vt_results.set_column_widths([376, 376])
 
     def sample_loaded_event(self, binary_buffer: bytes) -> None:
         self.__binary_buffer = binary_buffer
@@ -76,13 +82,16 @@ class VirusTotalIntegration:
             if element_alias == 'BUTTON_VT_ANALYZE':
                 tk_object.config(command=self.__vt_analyze_pressed)
             elif element_alias == 'TABLE_VT_RESULTS':
-                self.__tk_table_vt_results = element.get()
+                self.__table_vt_results = element.get()
             elif element_alias == 'LABEL_VT_RESULT':
-                self.__tk_label_vt_result = element.get()
+                self.__label_vt_result = element.get()
             elif element_alias == 'LOADING_LAYER':
                 self.__loading_layer = element.get()
 
-        self.__tk_table_vt_results.set_column_widths([300, 300])
+        self.__table_vt_results.set_column_widths([376, 376])
+
+        self.__table_vt_results.get_sheet_object().hide(canvas="x_scrollbar")
+        self.__table_vt_results.get_sheet_object().show(canvas="y_scrollbar")
 
     def request_needed_elements(self) -> list:
         return [
